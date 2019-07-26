fcbfcf21e656008972ebcc917246a38dea5446b4
Alfredo Diaz <mail4alfredo@gmail.com>
2019-07-26 22:48:35 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -10,6 +10,6 @@ def introduction_with_language_optional(name = "Alfredo", language = "Python")
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
-def introduction_with_language(name = "Alfredo", language = "Ruby")
+def introduction_with_language_optional(name = "Alfredo", language = "Ruby")
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
